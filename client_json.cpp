ce54246fe5dffeb2a399d962b26ea8c8358c9c2b
Richard D'Addio <rdaddio@akamai.com>
2016-11-16 20:06:33 -0500

    dev update

--- client_json.cpp
+++ client_json.cpp
@@ -4,6 +4,10 @@
 //json-c interface file
 
 //TEST JSON-c
+static int creat_user_table_entry(sqlit3 *db, std::string jstr);
+static std::string quotesql( const std::string& s ) {
+  return std::string("'") + s + std::string("'");
+}
 int handle_registration_json(std::string jstr)
 {
   sqlite3 *db = open_voc_db();
@@ -15,6 +19,21 @@ int handle_registration_json(std::string jstr)
   std::cout << "GOTTTTTTTTTT " << std::endl;
   int rc = create_user_table(db);
   std::cout << "create status: " << rc << std::endl;
-
+  rc = creat_user_table_entry(db, jstr); 
   close_voc_db(db);
+}
+
+static int creat_user_table_entry(sqlit3 *db, std::string jstr)
+{
+  json_object *new_obj = json_tokener_parse(jstr.c_str());
+  std::string userId = "test";
+  std::string password = "test";
+  string sqlstatement =
+    "INSERT INTO voc_user (my_row, userid, password, device_id, platform, device_type, access_token, refresh_token, voc_id, congestion_detection, ads_frequency, daily_quota, daily_manifest, max_content_duration, play_ads, skip_policy_first_time, tod_policy, token_expiration, server, server_state) VALUES ("
+    NONE,
+    + quotesql(userId) + ","
+    + quotesql(password) + ","
+    + quotesql() + ","
+    + quotesql(password) + ");";
+  
 }
