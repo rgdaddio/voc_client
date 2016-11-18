3d5d8b5466234106024e5cfb1f6170b8a9dcefbc
Richard D'Addio <rdaddio@akamai.com>
2016-11-17 19:48:05 -0500

    update

--- client_json.cpp
+++ client_json.cpp
@@ -4,10 +4,16 @@
 //json-c interface file
 
 //TEST JSON-c
-static int creat_user_table_entry(sqlit3 *db, std::string jstr);
-static std::string quotesql( const std::string& s ) {
+static int creat_user_table_entry(sqlite3 *db, std::string jstr);
+static std::string get_voc_id(json_object *j);
+static std::string get_platform(json_object *j);
+
+static std::string quotesql( const std::string& s ) 
+{
   return std::string("'") + s + std::string("'");
 }
+
+
 int handle_registration_json(std::string jstr)
 {
   sqlite3 *db = open_voc_db();
@@ -23,17 +29,31 @@ int handle_registration_json(std::string jstr)
   close_voc_db(db);
 }
 
-static int creat_user_table_entry(sqlit3 *db, std::string jstr)
+static int creat_user_table_entry(sqlite3 *db, std::string jstr)
 {
   json_object *new_obj = json_tokener_parse(jstr.c_str());
   std::string userId = "test";
   std::string password = "test";
-  string sqlstatement =
+  std::string sqlstatement =
     "INSERT INTO voc_user (my_row, userid, password, device_id, platform, device_type, access_token, refresh_token, voc_id, congestion_detection, ads_frequency, daily_quota, daily_manifest, max_content_duration, play_ads, skip_policy_first_time, tod_policy, token_expiration, server, server_state) VALUES ("
-    NONE,
-    + quotesql(userId) + ","
-    + quotesql(password) + ","
-    + quotesql() + ","
-    + quotesql(password) + ");";
-  
+    + quotesql(userId.c_str()) + ","
+    + quotesql(password.c_str()) + ","
+    + quotesql((get_voc_id(new_obj)).c_str()) + ","
+    + quotesql((get_platform(new_obj)).c_str()) + ");";
+  std::cout << "sql stmt: " << sqlstatement << std::endl;
+}
+
+static std::string get_voc_id(json_object *j)
+{
+  std::string rc;
+  std::cout << "vocid" << std::endl;
+  json_object *tmp = json_object_object_get(j, "vocId");
+  std::cout << json_object_get_string(tmp) << std::endl;
+  return (rc = json_object_get_string(tmp));
+}
+
+static std::string get_platform(json_object *j)
+{
+  std::string rc;
+  return (rc = "Linux");
 }
