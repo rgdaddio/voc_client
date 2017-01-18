5ab66fcead74b2c9dcf728467f3474e1561c2b4a
Richard D'Addio <rdaddio@akamai.com>
2017-01-17 20:00:16 -0500

    add separate https client

---
+++ https_client.h
@@ -0,0 +1,55 @@
+#ifndef HTTPS_CLIENT__H_
+#define HTTPS_CLIENT__H_
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/bind.hpp>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
+#include <string.h>
+#include "json/json.h"
+#include "json/json_object.h"
+#include <sqlite3.h>
+#include <string>
+#include <sstream>
+using boost::asio::ip::tcp;
+class https_client
+{
+  private:
+  boost::asio::ssl::stream<boost::asio::ip::tcp::socket> socket_;
+  boost::asio::ip::tcp::resolver::iterator citerator;
+  boost::asio::streambuf request_;
+  boost::asio::streambuf response_;
+  tcp::resolver resolver_;
+  std::string servr;
+  std::string cpath;
+  std::string cjson;
+  std::ostringstream sline;
+  
+ public:
+  https_client(boost::asio::io_service& io_service, boost::asio::ssl::context& context,
+	 boost::asio::ip::tcp::resolver::iterator endpoint_iterator, std::string server,std::string path);
+
+
+  bool verify_certificate(bool preverified, boost::asio::ssl::verify_context& ctx);
+  
+  void handle_resolve(const boost::system::error_code& err);
+  void handle_start(const boost::system::error_code& error);
+  void handle_connect(const boost::system::error_code& err);
+  void handle_write_request(const boost::system::error_code& err);
+  void handle_read_status_line(const boost::system::error_code& err);
+  void handle_read_headers(const boost::system::error_code& err, size_t bytes_transferred);
+  void handle_read_content(const boost::system::error_code& err, size_t bytes_transferred);
+  void build_http_get_header(std::string server_ip, std::string json);
+  //void build_http_post_header(std::string server_ip, std::string json);
+  //void build_http_get_header(std::string server_ip, std::string json);
+
+  boost::asio::streambuf& get_raw_response_strm(void)
+    {
+      return response_;
+    }
+};
+
+
+#endif
